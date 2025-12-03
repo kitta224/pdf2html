14526c248a9e4827d9c1cd2cd0aa0427eaa57c74
kitta224 <kitta.jimajima19@gmail.com>
2025-12-03 19:16:45 +0900

    リンクのパスを修正: index.htmlのアイコンパスをimage/icon-192.pngに更新、app.jsのservice worker登録パスをsw.jsに修正

--- js/app.js
+++ js/app.js
@@ -345,7 +345,7 @@ let deferredPrompt;
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('sw.js')
             .then((registration) => {
                 console.log('Service Worker registered successfully:', registration.scope);
             })
